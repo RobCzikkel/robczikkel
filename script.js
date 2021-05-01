040751deaaa6e98d1e55e02b9ba8ceabeeeefd7e
Robert Czikkel <robertczikkel@Roberts-MacBook-Pro.local>
2021-05-02 00:29:19 +0100

    Safari Check

--- script.js
+++ script.js
@@ -24,12 +24,49 @@ headerObserver.observe(header);
 
 
 // Smooth Scroll
+// document.querySelectorAll('.navlink').forEach(function(el) {
+//     el.addEventListener('click', function(e) {
+//         e.preventDefault();
+//         const id = this.getAttribute('href');
+//         document.querySelector(id).scrollIntoView({
+//             behavior: "smooth",
+//             block: 'start',
+//         })
+//     })
+// })
+
+
 document.querySelectorAll('.navlink').forEach(function(el) {
     el.addEventListener('click', function(e) {
         e.preventDefault();
         const id = this.getAttribute('href');
-        document.querySelector(id).scrollIntoView({behavior: "smooth"})
+
+        document.querySelector(id).getBoundingClientRect().top + nav.offsetHeight;
+
+        window.scrollTo({
+        left: document.querySelector(id).getBoundingClientRect().top + window.pageXOffset,
+        top: document.querySelector(id).getBoundingClientRect().top - (nav.offsetHeight * 2) + window.pageYOffset,
+        behavior: "smooth"
+    })
+        document.querySelector(id)
     })
 })
 
+// Tabbed content
+const tabs = document.querySelectorAll('.tab');
+const tabContainer = document.querySelector('.tabs');
+const tabArticles = document.querySelectorAll('.tabarticle');
 
+tabContainer.addEventListener('click', function(e) {
+    const clickedTab = e.target.closest('.tab');
+
+    if(!clickedTab) return;
+
+    //Active tab
+    tabs.forEach(t => t.classList.remove('light'))
+    clickedTab.classList.add('light')
+
+    //Active article
+    tabArticles.forEach(t => t.classList.remove('tabarticle--active'))
+    document.querySelector(`.content--${clickedTab.dataset.tab}`).classList.add('tabarticle--active')
+})
